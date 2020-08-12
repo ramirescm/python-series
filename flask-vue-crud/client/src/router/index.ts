b989ff69fe5c40be1eb1a7f3a7f7e05baad5d012
ramirescm <ramirescm@gmail.com>
2020-08-11 23:37:22 -0300

    flask with vue

---
+++ flask-vue-crud/client/src/router/index.ts
@@ -0,0 +1,42 @@
+import Vue from "vue";
+import VueRouter, { RouteConfig } from "vue-router";
+import Home from "../views/Home.vue";
+
+Vue.use(VueRouter);
+
+const routes: Array<RouteConfig> = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home
+  },
+  {
+    path: "/about",
+    name: "About",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/ping",
+    name: "Ping",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../components/Ping.vue")
+  },
+  {
+    path: "/books",
+    name: "Books",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../components/Books.vue")
+  }
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes
+});
+
+export default router;
